62ccd21ee79820817e24d4f133259688302edbf1
jsant12 <jsant12@wgu.edu>
2021-01-18 18:46:59 -0500

    Complete Iteration 8

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -6,9 +6,11 @@ const Celebrity = require("../models/celebrity.model");
 
 router.get("/movies/new", (req, res, next) => {
   Celebrity.find()
-    .then((celebritiesFromDB) => res.render("movies/new-movie.hbs", {
-      celebritiesFromDB
-    }))
+    .then((celebritiesFromDB) =>
+      res.render("movies/new-movie.hbs", {
+        celebritiesFromDB,
+      })
+    )
     .catch((err) => console.log(`Error displaying Celebrity info: ${err}`));
 });
 
@@ -21,9 +23,8 @@ router.post("/movies/create", (req, res, next) => {
 });
 
 router.get("/movies", (req, res, next) => {
-
   Movie.find()
-  .then((moviesFromDB) => res.render("movies/movies.hbs", { moviesFromDB }))
+    .then((moviesFromDB) => res.render("movies/movies.hbs", { moviesFromDB }))
 
     .catch((err) => console.log(`Error displaying new Movie: ${err}`));
 });
@@ -38,6 +39,30 @@ router.get("/movies/:id", (req, res, next) => {
     )
 
     .catch((err) => console.log(`Error displaying new Movie: ${err}`));
+});
+//================================================================================================
+router.post("/movies/:id/delete", (req, res, next) => {
+  Movie.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/movies"))
+    .catch((err) => console.log(`Error deleting Movie: ${err}`));
+});
+
+//================================================================================================
+
+router.get("/movies/:id/edit", (req, res, next) => {
+    Movie.findByIdAndUpdate(req.params.id)
+    .populate("cast", "name -_id")
+    .then((selectedMovie) => res.render("movies/edit-movie.hbs", {selectedMovie}))
+    .catch((err) => console.log(`Error getting update page Movie: ${err}`))
+
+});
+
+router.post("/movies/:id/edit", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.findByIdAndUpdate({ title, genre, plot, cast })
+    .then(res.redirect("/movies"))
+    .catch((err) => console.log(`Error creating new Movie: ${err}`));
+  res.redirect("back");
 });
 
 module.exports = router;
